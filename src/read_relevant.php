633d16713e4b06f5cae8bac1215a0de293a0fe2f
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-03-08 18:23:08 +0100

    first commit

---
+++ src/read_relevant.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+// #####################################################################################################################
+
+$temporaryPath = __DIR__ . "/storage";
+$reportFilePath = $temporaryPath . "/report.sqlite";
+
+// #####################################################################################################################
+
+$pdo = null;
+try {
+    $pdo = new PDO("sqlite:" . $reportFilePath);
+    $stmt = $pdo->query("SELECT DISTINCT url
+                         FROM examples
+                         WHERE is_relevant = 1");
+    foreach ($stmt as $index => $row) {
+        $url = $row["url"];
+        preg_match('#^https://www.php.net/manual/en/(.+)\.php$#', $url, $matches);
+        printf("%-60s", $matches[1]);
+        if ($index % 3 === 0) {
+            echo PHP_EOL;
+        }
+    }
+    $stmt = null;
+    unset($stmt);
+} catch (PDOException $e) {
+    echo $e->getMessage(), PHP_EOL;
+}
+$pdo = null;
+unset($pdo);
+
+echo PHP_EOL;
